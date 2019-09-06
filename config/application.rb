87d0327a2d352a0784899e3d6ca820a555f1bab0
yusuke matsuda <y.santamaria567@gmail.com>
2019-09-06 15:53:39 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module Manyou
+  class Application < Rails::Application
+    config.load_defaults 5.2
+
+    config.i18n.default_locale = :ja
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+end
