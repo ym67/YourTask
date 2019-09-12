fa1fc85cd2591f7c47e622060038736406caff59
yusuke matsuda <y.santamaria567@gmail.com>
2019-09-12 11:01:28 +0900

    i18nの複数ロケールファイルが読み込まれるようapplication.rbにpathを追記

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ module Manyou
     config.load_defaults 5.2
 
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
