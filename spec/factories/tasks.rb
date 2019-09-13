54b43b68b9e0c202bab04839ff5c8a751e5dd35e
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-13 11:12:45 +0900

    tasks.rbにcreated_atの値を追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :task_test1, class: Task do
     name { 'テスト1:タスク名' }
     description { 'テスト1:説明' }
+    created_at { Time.current + 1.days }
   end
 
   factory :task_test2, class: Task do
     name { 'テスト2:タスク名' }
     description { 'テスト2:説明' }
+    created_at { Time.current + 2.days }
   end
 end
