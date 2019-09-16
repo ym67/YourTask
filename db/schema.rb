fe6c952ae4c6691e0f6b76a95fd138661be356b4
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-16 16:28:07 +0900

    deadline(終了期限)カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_074837) do
+ActiveRecord::Schema.define(version: 2019_09_16_065215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_13_074837) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline", default: -> { "now()" }, null: false
   end
 
 end
