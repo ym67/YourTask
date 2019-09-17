ba10c4a39a8f90d94d6bb553c6d72deee121d123
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-17 13:44:31 +0900

    statusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_065215) do
+ActiveRecord::Schema.define(version: 2019_09_17_043830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_065215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: -> { "now()" }, null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
