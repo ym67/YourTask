010069b07e90baad4a7e3b23dd9ef6439370e352
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-18 10:34:30 +0900

    taskモデルにstatusのenumを定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
 
   validates :name, length: { in: 1..30 }
 
-  enum status: { "未着手": 0, "着手中": 1, "完了": 2, }
+  enum status: { untouched: 0, in_progress: 1, done: 2, }
 
 end
