c36efa443880126d01672477d4ac69dafb2dc466
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-18 15:03:39 +0900

    priorityのenumを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :name, length: { in: 1..30 }
 
   enum status: { untouched: 0, in_progress: 1, done: 2, }
+  enum priority: { high: 0, middle: 1, low: 2, }
 
   scope :sorted, -> { order(created_at: :desc) }
 
