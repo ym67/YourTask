28e4a3c9cc2451d58cf3876d3ac6fe9b0f42e858
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-20 14:28:44 +0900

    userモデルのバリデーションを一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,15 @@
 class User < ApplicationRecord
 
   validates :name,
-    presence: true,
     length: { in: 1..50 }
 
   validates :email,
-    presence: true,
     length: { in: 1..255 },
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
     uniqueness: true
 
   has_secure_password
   validates :password,
-    presence: true,
     length: { minimum: 6 }
 
 end
