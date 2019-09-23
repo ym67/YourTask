46f1e50931eb9ec4ea149f96ba039c27ee74e67a
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-23 13:13:33 +0900

    Taskモデルのテストを更新

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   it "nameが空ならバリデーションが通らない" do
-    task = Task.new(name: '', description: '失敗テスト')
+    FactoryBot.create(:test_user1)
+    task = Task.new(name: '', description: '失敗テスト', user_id: 1)
     expect(task).not_to be_valid
   end
 
-  it "nameとdescriptionに内容が記載されていればバリデーションが通る" do
-    task = Task.new(name: 'テスト！', description: '成功テスト')
+  it "nameが記載されていればバリデーションが通る" do
+    FactoryBot.create(:test_user1)
+    task = Task.new(name: 'テスト！', description: '成功テスト', user_id: 1)
     expect(task).to be_valid
   end
 
   it "scopeのテスト" do
-    FactoryBot.create(:task_test1)
-    FactoryBot.create(:task_test2)
-    FactoryBot.create(:task_test3)
+    FactoryBot.create(:test_user1)
+    FactoryBot.create(:test_task1)
+    FactoryBot.create(:test_task2)
     tasks = Task.all
     tasks_without_scope = tasks.order(created_at: :desc)
     tasks_with_scope = tasks.sorted
