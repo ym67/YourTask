f1059e67f80249655fda4505a3b0f2c0de64b8f9
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-23 12:23:23 +0900

    taskのfeature specを更新

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,29 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :test_user1, class: User do
+    id { 1 }
+    name { 'テストユーザ1' }
+    email { 'test1@example.com' }
+    password { 'test111'}
+    created_at { DateTime.current }
+    updated_at { DateTime.current }
   end
+
+  factory :test_user2, class: User do
+    id { 2 }
+    name { 'テストユーザ2' }
+    email { 'test2@example.com' }
+    password { 'test222'}
+    created_at { DateTime.current }
+    updated_at { DateTime.current }
+  end
+
+  factory :test_user3, class: User do
+    id { 3 }
+    name { 'テストユーザ3' }
+    email { 'test3@example.com' }
+    password { 'test333'}
+    created_at { DateTime.current }
+    updated_at { DateTime.current }
+  end
+
 end
