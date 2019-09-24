addb2a36efba74ec763c3a5fe89556acde1cbc7a
Yusuke Matsuda <y.santamaria567@gmail.com>
2019-09-24 16:47:07 +0900

    seedデータを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 # conding: utf-8
 
-User.create(name: 'シード1', email: 'seed1@seed.com', password: 'seed111', created_at: DateTime.current, updated_at: DateTime.current)
+User.create(name: '一般ユーザ', email: 'normal@seed.com', password: 'seed111', created_at: DateTime.current, updated_at: DateTime.current, admin: false)
+
+User.create(name: 'adminユーザ', email: 'admin@seed.com', password: 'admin111', created_at: DateTime.current, updated_at: DateTime.current, admin: true)
